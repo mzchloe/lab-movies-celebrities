a369b411d3b1223c4b9a30315b631b5b3ee50930
Jeanet Lin <linett@gmail.com>
2022-01-31 17:10:07 +0100

    iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
-//  Add your code here
+//  Iteration 2 Celebrity Model
+const mongoose = require('mongoose')
+
+const celebritySchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    occupation: {
+        type: String
+    }, 
+    catchPhrase: {
+        type: String
+    }
+})
+
+//export the model 
+module.exports = mongoose.model('Celebrity', celebritySchema)
